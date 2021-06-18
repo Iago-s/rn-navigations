ee7f72ed4beb3e6f3b0e6de6451fd64c2f379113
Iago Silva Oliveira <iagooliveira3845@hotmail.com>
2021-06-18 11:27:14 -0300

    feat: Navigations

--- App.js
+++ App.js
@@ -1,21 +1,158 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useMemo } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-export default function App() {
+import {
+  Splash,
+  SignIn,
+  CreateAccount,
+  Profile,
+  Home,
+  Search,
+  Details,
+  Search2,
+} from './Screens';
+
+import { AuthContext } from './context';
+
+const RootStack = createStackNavigator();
+const RootStackScreen = ({ userToken }) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <RootStack.Navigator headerMode="none">
+      {userToken ? (
+        <RootStack.Screen
+          name="App"
+          component={DrawerScreen}
+          options={{
+            animationEnabled: false,
+          }}
+        />
+      ) : (
+        <RootStack.Screen
+          name="Auth"
+          component={AuthStackScreen}
+          options={{
+            animationEnabled: false,
+          }}
+        />
+      )}
+    </RootStack.Navigator>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Drawer = createDrawerNavigator();
+const DrawerScreen = () => {
+  return (
+    <Drawer.Navigator initialRouteName="Profile">
+      <Drawer.Screen name="Home" component={TabsScreen} />
+      <Drawer.Screen name="Profile" component={ProfileStackScreen} />
+    </Drawer.Navigator>
+  );
+};
+
+const Tabs = createBottomTabNavigator();
+const TabsScreen = () => {
+  return (
+    <Tabs.Navigator>
+      <Tabs.Screen name="Home" component={HomeStackScreen} />
+      <Tabs.Screen name="Search" component={SearchStackScreen} />
+    </Tabs.Navigator>
+  );
+};
+
+const HomeStack = createStackNavigator();
+const HomeStackScreen = () => {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={Home} />
+      <HomeStack.Screen
+        name="Details"
+        component={Details}
+        options={({ route }) => ({
+          title: route.params.name,
+        })}
+      />
+    </HomeStack.Navigator>
+  );
+};
+
+const SearchStack = createStackNavigator();
+const SearchStackScreen = () => {
+  return (
+    <SearchStack.Navigator>
+      <SearchStack.Screen name="Search" component={Search} />
+      <SearchStack.Screen name="Search2" component={Search2} />
+    </SearchStack.Navigator>
+  );
+};
+
+const ProfileStack = createStackNavigator();
+const ProfileStackScreen = () => {
+  return (
+    <ProfileStack.Navigator>
+      <ProfileStack.Screen name="Profile" component={Profile} />
+    </ProfileStack.Navigator>
+  );
+};
+
+const AuthStack = createStackNavigator();
+const AuthStackScreen = () => {
+  return (
+    <AuthStack.Navigator>
+      <AuthStack.Screen
+        name="SignIn"
+        component={SignIn}
+        options={{ title: 'Sign In' }}
+      />
+      <AuthStack.Screen
+        name="CreateAccount"
+        component={CreateAccount}
+        options={{
+          title: 'Create Account',
+        }}
+      />
+    </AuthStack.Navigator>
+  );
+};
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [userToken, setUserToken] = useState(null);
+
+  const authContext = useMemo(() => {
+    return {
+      signIn: () => {
+        setIsLoading(false);
+        setUserToken('123abc');
+      },
+      signUp: () => {
+        setIsLoading(false);
+        setUserToken('123abc');
+      },
+      signOut: () => {
+        setIsLoading(false);
+        setUserToken(null);
+      },
+    };
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => setIsLoading(false), 1000);
+  }, []);
+
+  if (isLoading) {
+    return <Splash />;
+  }
+
+  return (
+    <AuthContext.Provider value={authContext}>
+      <NavigationContainer>
+        <RootStackScreen userToken={userToken} />
+      </NavigationContainer>
+    </AuthContext.Provider>
+  );
+};
+
+export default App;
